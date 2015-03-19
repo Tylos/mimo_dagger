5a71c3557bc1ec2aec23ac4d7345a68f84ae7891
Tylos <grtylos@gmail.com>
2015-03-19 08:15:07 +0100

    Refactor LoginActivity
    Remove the old code and introduce the use of the new LoginApi class,
    making easier to understand what is going on.

--- app/src/main/java/com/example/tylos/daggermimo/login/view/LoginActivity.java
+++ app/src/main/java/com/example/tylos/daggermimo/login/view/LoginActivity.java
@@ -7,20 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.tylos.daggermimo.BuildConfig;
 import com.example.tylos.daggermimo.R;
 import com.example.tylos.daggermimo.base.view.BaseActivity;
-import com.example.tylos.daggermimo.login.api.model.Account;
-import com.example.tylos.daggermimo.login.api.model.RequestToken;
+import com.example.tylos.daggermimo.login.api.LoginApi;
 import com.example.tylos.daggermimo.login.api.model.SessionData;
-import com.example.tylos.daggermimo.login.api.model.SessionToken;
-import com.example.tylos.daggermimo.login.api.services.AuthenticationService;
-import com.example.tylos.daggermimo.movie.api.services.AccountMovieService;
 import com.example.tylos.daggermimo.upcomming.view.MoviesListActivity;
-
-import retrofit.RequestInterceptor;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
 
 public class LoginActivity extends BaseActivity {
     private EditText username;
@@ -51,73 +42,38 @@ public class LoginActivity extends BaseActivity {
     }
 
     public void onLoginClick(View view) {
-        new AsyncTask<Void, Void, SessionData>() {
+        new AsyncTask<Void, Void, Boolean>() {
             @Override
-            protected SessionData doInBackground(Void... params) {
-                AuthenticationService service = new RestAdapter.Builder()
-                        .setEndpoint(BuildConfig.TMDB_ROOT)
-                        .setRequestInterceptor(new RequestInterceptor() {
-                            @Override
-                            public void intercept(RequestFacade request) {
-                                request.addQueryParam("api_key", BuildConfig.TMDB_API_KEY);
-                            }
-                        })
-                        .setLogLevel(RestAdapter.LogLevel.FULL)
-                        .build()
-                        .create(AuthenticationService.class);
+            protected Boolean doInBackground(Void... params) {
+                LoginApi api = new LoginApi();
 
-
-                RequestToken token;
-                SessionToken session;
-                try {
-                    token = service.generateNewRequestToken();
-                    token = service.validateTokenWithLogin(
-                            token.getRequestToken(),
-                            username.getText().toString(),
-                            password.getText().toString()
-                    );
-                    session = service.generateSessionToken(token.getRequestToken());
-                } catch (RetrofitError error) {
-                    session = null;
+                SessionData session = api.login(username.getText().toString(), password.getText().toString());
+                if (isSessionValid(session)) {
+                    storeSession(session);
+                    return Boolean.TRUE;
                 }
 
-                Account account = null;
-                final SessionToken finalSession = session;
-                if (session != null) {
-                   try {
-                       AccountMovieService accountService = new RestAdapter.Builder()
-                               .setEndpoint(BuildConfig.TMDB_ROOT)
-                               .setRequestInterceptor(new RequestInterceptor() {
-                                   @Override
-                                   public void intercept(RequestFacade request) {
-                                       request.addQueryParam("api_key", BuildConfig.TMDB_API_KEY);
-                                       request.addQueryParam("session_id", finalSession.getRequestToken());
-                                   }
-                               })
-                               .setLogLevel(RestAdapter.LogLevel.FULL)
-                               .build()
-                               .create(AccountMovieService.class);
-                       account = accountService.getAccountDetails();
-                   }catch (RetrofitError error) {
-                       account = null;
-                   }
-                }
-
-                return new SessionData(session, account);
+                return Boolean.FALSE;
             }
 
-
             @Override
-            protected void onPostExecute(SessionData session) {
-                if (session.getToken()!=null && session.getAccount() != null && session.getToken().isSuccess()) {
-                    SharedPreferences preferences = LoginActivity.this.getApplication().getSharedPreferences("mimodagger", MODE_PRIVATE);
-                    preferences.edit().putString("preferences.session_token", session.getToken().getRequestToken()).commit();
-                    preferences.edit().putLong("preferences.account_id", session.getAccount().getId()).commit();
+            protected void onPostExecute(Boolean isSuccess) {
+                if (isSuccess) {
                     startActivity(new Intent(LoginActivity.this, MoviesListActivity.class));
                 }
 
             }
         }.execute();
+    }
+
+    private void storeSession(SessionData session) {
+        SharedPreferences preferences = this.getApplication().getSharedPreferences("mimodagger", MODE_PRIVATE);
+        preferences.edit().putString("preferences.session_token", session.getToken().getRequestToken()).commit();
+        preferences.edit().putLong("preferences.account_id", session.getAccount().getId()).commit();
+    }
+
+    private boolean isSessionValid(SessionData session) {
+        return session.getToken() != null && session.getAccount() != null && session.getToken().isSuccess();
     }
 
 }
